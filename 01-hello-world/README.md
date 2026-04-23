b4af5d961057a2d2e07ca096e86eb379ef050090
Pat Hickey <p.hickey@f5.com>
2026-04-23 13:15:42 -0700

    add 01-hello-world

---
+++ 01-hello-world/README.md
@@ -0,0 +1,7 @@
+# 01-hello-world
+
+This is the simplest possible program that can run in our programmability
+solution: an HTTP server that, no matter what request it recieves, always
+returns a 200 OK response with tbe body "Hello World!\n".
+
+
---
+++ 01-hello-world/src/main.rs
@@ -0,0 +1,21 @@
+use wstd::http::{Request, Response, Body, Error};
+
+#[wstd::http_server]
+async fn main(_req: Request<Body>) -> Result<Response<Body>, Error> {
+    // Contents of the body are just a str here.
+    let resp_body_contents: &str = "Hello, world!\n";
+    // Convert those contents into a Body. 
+    let resp_body: Body = resp_body_contents.into();
+    // Create a Response that contains that Body.
+    let resp = Response::new(resp_body);
+    // Return it in Ok, because this function was successful. If you returned
+    // an error, the HTTP Server would hang up.
+    Ok(resp)
+
+
+    // Ideas for further exploration:
+    // * Many different things can convert to a Body:
+    //   https://docs.rs/wstd/latest/wstd/http/struct.Body.html#trait-implementations
+    // * You could instead return an error, replacing the Ok with 
+    //   `Err(Error::msg("something terrible occured"))`
+}
