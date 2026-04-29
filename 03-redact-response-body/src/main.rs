ef79e47018919e2e1f38de9f310097c0e32b6cb0
Pat Hickey <p.hickey@f5.com>
2026-04-28 17:03:29 -0700

    fix bug, rustfmt

--- 03-redact-response-body/src/main.rs
+++ 03-redact-response-body/src/main.rs
@@ -1,5 +1,5 @@
-use wstd::http::{Body, Error, Request, Response, Client};
 use serde::{Deserialize, Serialize};
+use wstd::http::{Body, Client, Error, Request, Response};
 
 #[wstd::http_server]
 async fn main(_req: Request<Body>) -> Result<Response<Body>, Error> {
@@ -13,7 +13,9 @@ async fn main(_req: Request<Body>) -> Result<Response<Body>, Error> {
     // indeed, fake.)
     //
     // This demo is writing a filter which redacts those SSNs in the response.
-    let mut people_resp = Client::new().send(Request::new("http://10.1.1.4:8001/people.json")).await?;
+    let mut people_resp = Client::new()
+        .send(Request::get("http://10.1.1.4:8001/people.json").body(())?)
+        .await?;
 
     // /people.json returns a JSON structure where there is a list of dicts,
     // and each dict has string fields `firstname`, `lastname`, `city`, `ssn`.
@@ -21,7 +23,12 @@ async fn main(_req: Request<Body>) -> Result<Response<Body>, Error> {
     // those fields, and the `#[derive(Serialize, Deserialize)]` means serde
     // will know how to decode that struct from JSON and encode it to JSON.
     #[derive(Serialize, Deserialize)]
-    struct Person { firstname: String, lastname: String, city: String, ssn: String }
+    struct Person {
+        firstname: String,
+        lastname: String,
+        city: String,
+        ssn: String,
+    }
 
     // We can collect the body in the response and then decode it from JSON
     // into a list of persons:
