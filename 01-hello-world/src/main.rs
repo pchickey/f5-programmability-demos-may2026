350aa870b6046aea4a7bcd2c95ca55f886fa7d81
Pat Hickey <p.hickey@f5.com>
2026-04-28 17:02:57 -0700

    clarify docs, rustfmt

--- 01-hello-world/src/main.rs
+++ 01-hello-world/src/main.rs
@@ -1,10 +1,10 @@
-use wstd::http::{Request, Response, Body, Error};
+use wstd::http::{Body, Error, Request, Response};
 
 #[wstd::http_server]
 async fn main(_req: Request<Body>) -> Result<Response<Body>, Error> {
     // Contents of the body are just a str here.
     let resp_body_contents: &str = "Hello, world!\n";
-    // Convert those contents into a Body. 
+    // Convert those contents into a Body.
     let resp_body: Body = resp_body_contents.into();
     // Create a Response that contains that Body.
     let resp = Response::new(resp_body);
@@ -12,10 +12,13 @@ async fn main(_req: Request<Body>) -> Result<Response<Body>, Error> {
     // an error, the HTTP Server would hang up.
     Ok(resp)
 
-
     // Ideas for further exploration:
     // * Many different things can convert to a Body:
     //   https://docs.rs/wstd/latest/wstd/http/struct.Body.html#trait-implementations
-    // * You could instead return an error, replacing the Ok with 
-    //   `Err(Error::msg("something terrible occured"))`
+    // * You could instead return an error, replacing the Ok with
+    //   `Err(Error::msg("something terrible occured"))`.
+    //   When Wasm returns an Error to NGINX instead of a Response, NGINX
+    //   itself will respond with an empty 500, and log the contents of the
+    //   Error. You'll need to look in the nginx error logs to debug the
+    //   Error.
 }
